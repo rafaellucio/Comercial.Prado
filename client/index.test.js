d1149b4be1eda6f82a27b9b2af0d847365e5543b
Rafael Antonio Lucio <rafaelantoniolucio@gmail.com>
2017-07-16 23:07:56 -0300

    Try configure istanbul to coverage tests

--- client/index.test.js
+++ client/index.test.js
@@ -1,9 +1,13 @@
-'use strict';
-
 const assert = require('assert');
+
+const sum = require('./index');
 
 describe('Mocha expect', () => {
   it('Assert Client Unit Tests', () => {
     assert.equal(true, true);
+  });
+
+  it('Shold sum two values', () => {
+    assert.equal(sum(10, 20), 30);
   });
 });
