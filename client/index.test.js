e4a2a7a103de5b8ba22b65a9b68ebb5df54ea747
Rafael Antonio Lucio <rafaelantoniolucio@gmail.com>
2017-07-16 23:57:36 -0300

    Fix coverage istanbul and include simulate tests

--- client/index.test.js
+++ client/index.test.js
@@ -8,6 +8,18 @@ describe('Mocha expect', () => {
   });
 
   it('Shold sum two values', () => {
-    // assert.equal(calculator.exec('sum', 10, 20), 30);
+    assert.equal(calculator.exec('sum', 10, 20), 30);
+  });
+
+  it('Shold subtract two values', () => {
+    assert.equal(calculator.exec('sub', 10, 20), -10);
+  });
+
+  it('Shold sum 10 + 10 whem no pass name operation', () => {
+    assert.equal(calculator.exec(undefined, 10, 20), 20);
+  });
+
+  it('Shold mult two values', () => {
+    assert.equal(calculator.mult(10, 20), 200);
   });
 });
